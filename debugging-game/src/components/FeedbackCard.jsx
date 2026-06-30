ae4b7c220ab8ba8a9c71921e7e2141d58ff77ae3
Gabriela Smith Ferreira <142529563+GabiiSmithh@users.noreply.github.com>
2026-06-30 19:51:18 -0300

    Delete debugging-game directory

--- debugging-game/src/components/FeedbackCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function FeedbackCard({ title, children, variant = 'info' }) {
-  return (
-    <article className={`feedbackCard ${variant}`}>
-      <h3>{title}</h3>
-      <div>{children}</div>
-    </article>
-  );
-}
